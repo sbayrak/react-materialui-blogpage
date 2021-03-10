77de71a86c97d354185884ac6e21d977786db992
Suat Bayrak <suat.bayrak@bilgiedu.net>
2021-03-10 11:19:02 +0300

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { AppBar, Toolbar, Typography } from '@material-ui/core';
-import theme from './theme';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
